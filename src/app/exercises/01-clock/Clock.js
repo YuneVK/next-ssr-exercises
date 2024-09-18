4943609a19ce88c8f8b5b2a89154928774989800
Sonia Ruiz <yune.vk@gmail.com>
2024-09-18 11:40:30 +0200

    Exercise 1: add suppressHydrationWarning

--- src/app/exercises/01-clock/Clock.js
+++ src/app/exercises/01-clock/Clock.js
@@ -1,23 +1,25 @@
-'use client';
-import React from 'react';
-import format from 'date-fns/format';
+"use client"
+import React from "react"
+import format from "date-fns/format"
 
 function Clock() {
-  const [time, setTime] = React.useState(new Date());
+  const [time, setTime] = React.useState(new Date())
 
   React.useEffect(() => {
     const intervalId = window.setInterval(() => {
-      setTime(new Date());
-    }, 50);
+      setTime(new Date())
+    }, 50)
 
     return () => {
-      window.clearInterval(intervalId);
-    };
-  }, []);
+      window.clearInterval(intervalId)
+    }
+  }, [])
 
   return (
-    <p className="clock">{format(time, 'hh:mm:ss.S a')}</p>
-  );
+    <p suppressHydrationWarning className="clock">
+      {format(time, "hh:mm:ss.S a")}
+    </p>
+  )
 }
 
-export default Clock;
+export default Clock
